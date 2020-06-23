cb5a3b61912585145f22b2baca80dadb8bdfb6a0
trinm31 <trinmgcd19833@fpt.edu.vn>
2020-06-23 20:15:31 +0700

    updat data

--- crudbase/classes/database.php
+++ crudbase/classes/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database {
     // Connection variables
-    private $host = "localhost";
-    private $dbName = "ATN_storage";
-    private $username = "root";
-    private $password = "";
+    private $host = "ec2-50-17-90-177.compute-1.amazonaws.com";
+    private $dbName = "d302jgs2ffsb8q";
+    private $username = "tuftbcymifesjs";
+    private $password = "9030dfe165c9a3aa812ceed5d08e5a0ed7eeb5787eb73ea6bc072e255b99cf57";
 
     public $conn;
 
